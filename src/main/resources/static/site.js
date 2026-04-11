ddda8956810d4385f6b8e9655d15d4450532dde0
23PA1A05E0 <magulurinani916@gmail.com>
2026-04-11 23:28:58 +0530

    Working with API

--- src/main/resources/static/site.js
+++ src/main/resources/static/site.js
@@ -2,23 +2,45 @@ var john = true
 function showNextUser(){
     if(john == true){
         document.getElementById("userName").innerHTML = "jane_doe"
-        document.getElementById("designation").innerHTML = "Interior Designer"
+        document.getElementById("email").innerHTML = "Interior Designer"
         document.getElementById("userImage").src = "./img_jane_doe.png"
         john = false
     }
     else{
         document.getElementById("userName").innerHTML = "john_doe"
-        document.getElementById("designation").innerHTML = "Architect & Engineer"
+        document.getElementById("email").innerHTML = "Architect & Engineer"
         document.getElementById("userImage").src = "./img_john_doe.png"
         john = true;
     }
 }
 
-function randomUser(){
-    //https://randomuser.me/api call this api
-    // Show userName, Designation in HTML 
-    // If user is male show John_Doe img
-    // If user is female show Jane_Doe img
-    // Then show actual img of user coming from api
 
+async function showRandomUser() {
+    try {
+        let res = await fetch("https://randomuser.me/api/");
+        let data = await res.json();
+
+        let user = data.results[0];
+
+        document.getElementById("userName").innerHTML =
+            user.name.first + " " + user.name.last;
+
+        document.getElementById("email").innerHTML =
+            user.email;
+
+        document.getElementById("userImage").src =
+            user.picture.large;
+
+    } catch (error) {
+        console.log("Error:", error);
+    }
 }
+
+// function randomUser(){
+//     //https://randomuser.me/api call this api
+//     // Show userName, Designation in HTML 
+//     // If user is male show John_Doe img
+//     // If user is female show Jane_Doe img
+//     // Then show actual img of user coming from api
+
+// }
