536276d4b26862a8d38588f9eaf4908dde2629ef
23PA1A05E0 <magulurinani916@gmail.com>
2026-04-11 22:51:04 +0530

    Second

--- src/main/resources/static/site.js
+++ src/main/resources/static/site.js
@@ -3,11 +3,13 @@ function showNextUser(){
     if(john == true){
         document.getElementById("userName").innerHTML = "jane_doe"
         document.getElementById("designation").innerHTML = "Interior Designer"
+        document.getElementById("userImage").src = "./img_jane_doe.png"
         john = false
     }
     else{
         document.getElementById("userName").innerHTML = "john_doe"
         document.getElementById("designation").innerHTML = "Architect & Engineer"
+        document.getElementById("userImage").src = "./img_john_doe.png"
         john = true;
     }
 }
